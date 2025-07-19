0f31f321d65d1d4dc6a0e50001b1fe15009362cc
Muhammed Emir IŞIK <03emire@gmail.com>
2025-07-19 20:01:55 +0300

    Update rswatch.js

--- commands/rswatch.js
+++ commands/rswatch.js
@@ -6,10 +6,9 @@ module.exports = {
         .setDescription('SitWatchdaki Videolardan Birini Rasgere Açar'),
     async execute(interaction) {
         const json = (await fetch('https://sitwatch.net/api/videos/latest?page=1&limit=1')).json();
-        const videoId = json[0].id;
-        const randomVideoId = Math.floor(Math.random() * videoId + 1);
+        const videoId = json[0].watch_id;
 
-        const videoUrl = `https://sitwatch.net/watch/${randomVideoId}`;
+        const videoUrl = `https://sitwatch.net/watch/${videoId}`;
         const embed = new EmbedBuilder()
             .setTitle('Rasgere Watch')
             .setDescription('Rasgere Watch, SitWatchdaki Videolardan Birini Rasgere Açar.')
